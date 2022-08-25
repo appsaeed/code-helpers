0c4a7a7dffb155cfa8f24341fe144c66c1310ed9
Saeed <78552486+appsaeed@users.noreply.github.com>
2022-08-26 00:47:06 +0600

    Create copy-to-clipboard.js

---
+++ copy-to-clipboard.js
@@ -0,0 +1,71 @@
+@php
+$content = file_get_contents(public_path('/') . config('app.logo'));
+$path = public_path('/') . config('app.logo');
+@endphp
+
+<button class="btn">Copy Picture!</button>
+
+
+<script>
+    async function copyPicture(url){
+        try {
+            const response = await fetch(url);
+            const blob = await response.blob();
+            await navigator.clipboard.write([
+                new ClipboardItem({
+                    [blob.type]: blob
+                })
+            ]);
+            console.log('Image copied.');
+        } catch (err) {
+            console.error(err.name, err.message);
+        }
+    };
+
+
+    const button = document.querySelector('.btn');
+    button.addEventListener('click', function() {
+
+            copyPicture('http://localhost/application.com/qrcode/63069e6925cb1/vie')
+        
+
+
+    })
+
+
+    function imageToBlob(imageURL) {
+        const img = new Image;
+        const c = document.createElement("canvas");
+        const ctx = c.getContext("2d");
+        img.crossOrigin = "";
+        img.src = imageURL;
+        return new Promise(resolve => {
+            img.onload = function() {
+                c.width = this.naturalWidth;
+                c.height = this.naturalHeight;
+                ctx.drawImage(this, 0, 0);
+                c.toBlob((blob) => {
+                    // here the image is a blob
+                    resolve(blob)
+                }, "image/png", 0.75);
+            };
+        })
+    }
+
+    imageToBlob('http://localhost/application.com/qrcode/63069e6925cb1/view')
+
+    // try {
+
+    //     async function copyImage(imageURL) {
+    //         const blob = await imageToBlob(imageURL)
+    //         const item = new ClipboardItem({
+    //             "image/png": blob
+    //         });
+
+    //         navigator.clipboard.write([item]);
+    //     }
+    //     copyImage('http://localhost/application.com/qrcode/63069e6925cb1/view+');
+    // } catch (error) {
+    //     console.log(error)
+    // }
+</script>
