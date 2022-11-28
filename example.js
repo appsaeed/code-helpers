0b33668db7da9d0e9713047c268b23773270060f
Saeed <78552486+appsaeed@users.noreply.github.com>
2022-11-28 18:12:53 +0600

    Update example.js

--- example.js
+++ example.js
@@ -1,1 +1,16 @@
 
+function save(filename, data, type = "application/json") {
+  const blob = new Blob([data], { type: type });
+  if (window.navigator.msSaveOrOpenBlob) {
+    window.navigator.msSaveBlob(blob, filename);
+  } else {
+    const elem = window.document.createElement("a");
+    elem.href = window.URL.createObjectURL(blob);
+    elem.download = filename;
+    document.body.appendChild(elem);
+    elem.click();
+    document.body.removeChild(elem);
+  }
+}
+
+save("stateName2.json", JSON.stringify(stateName));
